bf748420e029c0e6b3cec042a4e3b40cce38d1d6
ananda kumar hn <andu1989anand@gmail.com>
2016-12-23 11:12:12 +0530

    Create quadratic.py

---
+++ quadratic.py
@@ -0,0 +1,10 @@
+a=float(input("enter value of a:"))
+b=float(input("enter value of b:"))
+c=float(input("enter value of c:"))
+
+disc=(b*b)-(4*a*c)
+
+if (disc==0):
+	print "roots are equal"
+else:
+	print "roots are not equal"
