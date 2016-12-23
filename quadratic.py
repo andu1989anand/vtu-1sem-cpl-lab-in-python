136bc469b58452850bbc5a5b5034050bfe82151e
ananda kumar hn <andu1989anand@gmail.com>
2016-12-23 12:01:23 +0530

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,3 +1,4 @@
+""" python program to find two roots of a quadratic equation """
 a=float(input("enter value of a:"))
 b=float(input("enter value of b:"))
 c=float(input("enter value of c:"))
@@ -5,6 +6,18 @@ c=float(input("enter value of c:"))
 disc=(b*b)-(4*a*c)
 
 if (disc==0):
-	print "roots are equal"
-else:
+	r1=r2=-b/(2*a)
+	print "roots are equal:",r1
+elif (disc >0):
 	print "roots are not equal"
+	r1=(-b+(disc**0.5))/(2*a)
+	r2=(-b-(disc**0.5))/(2*a)
+	print "root1:",r1
+	print "root2:",r2
+else:
+	print "roots are imaginary"
+	r=-b/(2*a)
+	i1=(abs(disc)**0.5)/(2*a)
+	i2=(abs(disc)**0.5)/(2*a)
+	print "root1:"+str(r)+"+i"+str(i1)
+	print "root2:"+str(r)+"-i"+str(i2)
